f7e125dd37792f7b59afe250263bb8e00839c3e3
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-19 19:50:59 +0100

    Scanner 1

---
+++ Scanner1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj liczbę naturalną: ");
+        int letter = scan.nextInt();
+        System.out.println("Oto wszystkie liczby naturalne mniejsze od " + letter + ": ");
+        for (int i = 1; i < letter; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
